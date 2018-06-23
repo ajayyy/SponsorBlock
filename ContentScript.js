efb172cc6c1e5a2537d6d7a6b61602c4c3bf5e2b
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2018-06-23 15:06:18 +0100

    Create ContentScript.js

---
+++ ContentScript.js
@@ -0,0 +1,31 @@
+var v = $('video')
+video_id = youtube_parser(document.URL);
+
+if(video_id){
+SponsorsLookup(video_id);
+}
+ 
+function SponsorsLookup(id){
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open('GET', 'https://officialnoob.github.io/YTSponsorSkip-Dataset/'+id, true);
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		Sponsors = JSON.parse(xmlhttp.responseText);
+		v.ontimeupdate = function() {SponsorCheck()};
+    }
+};
+xmlhttp.send(null);
+}
+ 
+function SponsorCheck() {
+    Sponsors.forEach(function(el, index) {
+      if((Math.floor(v.currentTime)) == el[0]){
+        v.currentTime = el[1];
+	  }
+});}
+
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
