872ea9fceff03d5576901a29b8d5c821c1c34fcc
Ajay Ramachandran <dev@ajay.app>
2019-07-09 22:52:59 -0400

    Changed the server URL to use new content-config.js file.

--- content.js
+++ content.js
@@ -74,7 +74,7 @@ function sponsorsLookup(id) {
     let xmlhttp = new XMLHttpRequest();
     
     //check database for sponsor times
-    xmlhttp.open('GET', 'http://localhost/api/getVideoSponsorTimes?videoID=' + id, true);
+    xmlhttp.open('GET', serverAddresss + "/api/getVideoSponsorTimes?videoID=" + id, true);
 
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
