ca4f91f39b1c176fefa359fd34ab268ba56018bc
Ajay Ramachandran <dev@ajay.app>
2019-07-11 14:00:03 -0400

    Made time have a zero in front when it is less than 10 seconds.

--- popup.js
+++ popup.js
@@ -241,6 +241,11 @@ function displayNoVideo() {
 function getFormattedTime(seconds) {
   let minutes = Math.floor(seconds / 60);
   let secondsDisplay = Math.round(seconds - minutes * 60);
+  if (secondsDisplay < 10) {
+    //add a zero
+    secondsDisplay = "0" + secondsDisplay;
+  }
+
   let formatted = minutes+ ":" + secondsDisplay;
 
   return formatted;
