5ee279dec7d074dd266c23892ff27b118f5bb504
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-04 19:06:07 +0100

    Added getYouTubeVideoStartTime

--- utils.js
+++ utils.js
@@ -9,3 +9,14 @@ function getYouTubeVideoID(url) { // Returns with video id else returns false
 
   return (match && match[7].length == 11) ? id : false;
 }
+
+//returns the start time of the video if there was one specified (ex. ?t=5s)
+function getYouTubeVideoStartTime(url) {
+  let searchParams = new URL(url).searchParams;
+  var startTime = searchParams.get("t");
+  if (startTime == null) {
+    startTime = searchParams.get("time_continue");
+  }
+
+  return startTime;
+}
