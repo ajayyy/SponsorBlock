7ad5e426fb965d4a06575377704244bd3c4dd2f3
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-06 10:27:13 +0100

    Improved Parser

--- utils.js
+++ utils.js
@@ -2,12 +2,10 @@ function getYouTubeVideoID(url) { // Returns with video id else returns false
   var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
   var match = url.match(regExp);
   var id = new URL(url).searchParams.get("v");
-  if (url.includes("/embed/")) {
-    //it is an embed, don't search for v
+  if (url.includes("/embed/")) { //it is an embed, don't search for v
     id = match[7];
   }
-
-  return (match && match[7].length == 11) ? id : false;
+   return (match) ? id : false;
 }
 
 //returns the start time of the video if there was one specified (ex. ?t=5s)
