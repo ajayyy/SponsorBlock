ba55cb1ac10bc146236c6c8bc2c221c3dc6f677e
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-07 23:02:58 +0100

    Added checks

--- UnitTest.js
+++ UnitTest.js
@@ -2,6 +2,8 @@
 // This should be automated via CI
 
 const Videos = [
+    { input: "https://www.youtube.com/watch?v=wJuWuJBbc1s#example", result: "wJuWuJBbc1s" },
+    { input: "https://www.youtube.com/watch?v=wJuWuJBbc1sX", result: "wJuWuJBbc1s" },
     { input: "https://www.youtube.com/watch?v=wJuWuJBbc1s", result: "wJuWuJBbc1s" },
     { input: "https://www.youtube.com/watch/?v=wJuWuJBbc1s", result: "wJuWuJBbc1s" },
     { input: "https://www.youtube.com/embed/wJuWuJBbc1s", result: "wJuWuJBbc1s" },
@@ -11,9 +13,11 @@ const Videos = [
     { input: "https://www.youtube.com/watch?time_continue=2&v=wJuWuJBbc1s", result: "wJuWuJBbc1s" },
     { input: "https://www.youtube.com/watch?time_continue=2&v=wJuWuJBbc1s&aaa=aaa", result: "wJuWuJBbc1s" },
     { input: "https://www.notyoutube.com/watch?v=wJuWuJBbc1s", result: false },
+    { input: "https://www.youtube.com.notyoutube.com/watch?v=wJuWuJBbc1s", result: false },
     { input: "https://www.notyoutube.com/embed/wJuWuJBbc1s", result: false },
     { input: "https://www.youtube.com/watch?v=badurl", result: false },
     { input: "https://www.youtube.com/embed/badurl", result: false },
+    { input: "https://www.youtube.com.notyoutube.com/embed/wJuWuJBbc1s", result: false },
     { input: "https://www.youtube-nocookie.com/embed/wJuWuJBbc1s", result: "wJuWuJBbc1s" },
     { input: "https://www.youtube-nocookie.com/watch?v=wJuWuJBbc1s", result: "wJuWuJBbc1s" }
 ];
