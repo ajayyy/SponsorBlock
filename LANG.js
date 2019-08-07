e5add6c392bb021c3d27d3b8a938d3b25762fb84
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-07 16:55:47 +0100

    Create LANG.js

---
+++ LANG.js
@@ -0,0 +1,9 @@
+  // File to store strings for diffrent languages
+  
+  var EN_US = new Map();
+  
+  EN_US.set(400, 'Server said this request was invalid"')
+     .set(429, 'You have submitted too many sponsor times for this one video, are you sure there are this many?')
+     .set(409, 'This has already been submitted before')
+     .set(502, 'It seems the server is down. Contact the dev to inform them.')
+     .set('Unknown', 'There was an error submitting your sponsor times, please try again later.');
