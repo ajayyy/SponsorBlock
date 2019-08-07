c7f8a392828f311afa7dab68473d9d828bd42c4e
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-07 17:45:10 +0100

    Update LANG.js

--- LANG.js
+++ LANG.js
@@ -17,8 +17,23 @@
   var en_US = new Map();
 
   // Main
-  en_US.set(400, 'Server said this request was invalid"')
-     .set(429, 'You have submitted too many sponsor times for this one video, are you sure there are this many?')
-     .set(409, 'This has already been submitted before')
-     .set(502, 'It seems the server is down. Contact the dev to inform them.')
-     .set('Unknown', 'There was an error submitting your sponsor times, please try again later.');
+en_US.set(400, 'Server said this request was invalid"')
+.set(429, 'You have submitted too many sponsor times for this one video, are you sure there are this many?')
+.set(409, 'This has already been submitted before')
+.set(502, 'It seems the server is down. Contact the dev to inform them.')
+.set('Unknown', 'There was an error submitting your sponsor times, please try again later.')
+.set("CWL","Channel Whitelisted!")
+.set("SPs","sponsors.")
+.set("SP","sponsor.")
+.set("SPSEGs","sponsor segments.")
+.set("SPSEG","sponsor segment.")
+.set("SP_FOUND","This video's sponsors are in the database!")
+.set("SP_NONE","No sponsors found)
+.set("SP_END","Sponsorship Ends Now")
+.set("SP_START","Sponsorship Starts Now")
+.set("NOTYTT","his probably isn't a YouTube tab, or you clicked too early. \n If you know this is a YouTube tab,\n close this popup and open it again.")
+.set("VOTE","Thanks for voting!")
+.set("VOTE_FAIL","You have already voted this way before.")
+.set("It seems the sever is down. Contact the dev immediately.","SBDOWN")
+
+
