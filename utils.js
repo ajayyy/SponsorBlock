1ce1bccc5950a642f1422e6bdbae1726ce56a4a7
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-13 17:01:13 +0100

    POC Parser

--- utils.js
+++ utils.js
@@ -1,37 +1,27 @@
-function getTrailerID() { // Requires DOM level access
-    let p = document.location.pathname;
-    if(!document.location.origin === "https://www.youtube.com") return false
-    if(!p.startsWith("/user/")  && !p.startsWith("/channel/"))  return false
-    if(document.getElementsByClassName("ytp-share-panel-link")[1] === undefined) return false
-    let id = document.getElementsByClassName("ytp-share-panel-link")[1].innerText.split(".be/")[1];
-    return id.length == 11 ? id : false;
-}
-
 function getYouTubeVideoID(url) {
-    //Attempt to parse url
-    let urlObject = null;
-    try { 
-        urlObject = new URL(url);
-    } catch (e) {      
-        console.error("[SB] Unable to parse URL: " + url);
-        return false
+    if(url === undefined) {
+        shere_url = document.getElementById("share-url");
+        if(shere_url) {
+            id = shere_url.split(".be/")[1];
+            return id.length == 11 ? id : false;
+        }
+        url = document.location;
+    } else {
+        try {
+            url = new URL(url);
+        } catch (e) {      
+            console.error("[SB] Unable to parse URL: " + url);
+            return false
+        }
     }
-  
-    //Check if valid hostname
-    if(!["www.youtube.com","www.youtube-nocookie.com"].includes(urlObject.host)) return false; 
-    
-    //Get ID from searchParam
-    if ((urlObject.pathname == "/watch" || urlObject.pathname == "/watch/") && urlObject.searchParams.has("v")) {
-      id = urlObject.searchParams.get("v"); 
-      return id.length == 11 ? id : false;
-    } else if (urlObject.pathname.startsWith("/embed/")) {
-      try {
-        return urlObject.pathname.substr(7, 11);
-      } catch (e) {
-        console.error("[SB] Video ID not valid for " + url);
-        return false;
-      }
-    }
+    if(url.pathname.startWith("/embed"))) {
+        try {
+            return url.pathname.substr(7, 11);
+        } catch (e) {
+            console.error("[SB] Video ID not valid for " + url);
+            return false;
+        }  
+        return id.length == 11 ? id : false;
 }
 
 //returns the start time of the video if there was one specified (ex. ?t=5s)
