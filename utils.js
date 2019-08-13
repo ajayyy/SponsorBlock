eaf02abc326fddc439ec97ed3d16be3a71f6af58
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-13 17:29:37 +0100

    Different way to get video ID in a content script

--- utils.js
+++ utils.js
@@ -1,12 +1,21 @@
 function getYouTubeVideoID(url) {
-    if(url === undefined) {
-        shere_url = document.getElementById("share-url");
-        if(shere_url) {
-            id = shere_url.split(".be/")[1];
-            return id.length == 11 ? id : false;
+    // If no url provided guess its in a contentscript
+    if (url === undefined) {
+        // Diffrent way works on channel trailer and watch?v=
+        let shere_url = document.getElementById("share-url");
+        if (shere_url) {
+            let id = shere_url.split(".be/")[1];
+            if(id.length == 11) return id;
         }
-        url = document.location;
-    } else {
+        url = document.location; // Already parsed :D
+    }
+    urlBasedParser(url);
+    }
+}
+
+function urlBasedParser(url) {
+    //Attempt to parse url
+    if(typeof url === 'string') {
         try {
             url = new URL(url);
         } catch (e) {      
@@ -14,14 +23,23 @@ function getYouTubeVideoID(url) {
             return false
         }
     }
-    if(url.pathname.startWith("/embed"))) {
-        try {
-            return url.pathname.substr(7, 11);
-        } catch (e) {
-            console.error("[SB] Video ID not valid for " + url);
-            return false;
-        }  
-        return id.length == 11 ? id : false;
+  
+    //Check if valid hostname
+    if(!["www.youtube.com","www.youtube-nocookie.com"].includes(url.host)) return false; 
+    
+    //Get ID from searchParam
+    if ((url.pathname == "/watch" || url.pathname == "/watch/") && url.searchParams.has("v")) {
+      id = url.searchParams.get("v"); 
+      return id.length == 11 ? id : false;
+    } else if (url.pathname.startsWith("/embed/")) {
+      try {
+        return url.pathname.substr(7, 11);
+      } catch (e) {
+        console.error("[SB] Video ID not valid for " + url);
+        return false;
+      }
+    }
+}
 }
 
 //returns the start time of the video if there was one specified (ex. ?t=5s)
