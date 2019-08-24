42dcdb40cf16f23abd90a7511de0bf5991ecdba0
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-08-24 18:06:25 +0100

    Added localizeHtmlPage

--- utils.js
+++ utils.js
@@ -39,3 +39,24 @@ function getYouTubeVideoID(url) {
     }
 	return false;
 }
+
+function localizeHtmlPage()
+{
+    //Localize by replacing __MSG_***__ meta tags
+    var objects = document.getElementsByTagName('html');
+    for (var j = 0; j < objects.length; j++)
+    {
+        var obj = objects[j];
+
+        var valStrH = obj.innerHTML.toString();
+        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
+        {
+            return v1 ? chrome.i18n.getMessage(v1) : "";
+        });
+
+        if(valNewH != valStrH)
+        {
+            obj.innerHTML = valNewH;
+        }
+    }
+}
