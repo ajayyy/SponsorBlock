8b655b62ac9e586f213d3f09d31d2a67ad4e4262
Jeremy Plsek <jeremyplsek@gmail.com>
2019-12-29 12:08:19 -0500

    Use eslint for js formatting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        webextensions: true
+    },
+    extends: [
+        "standard"
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        indent: ["error", 4],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "object-curly-spacing": ["error", "never"],
+        "space-before-function-paren": ["error", "never"]
+    }
+};
