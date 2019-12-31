272698f97bb9915a72171c15df0de46c7f27890d
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-12-31 19:03:43 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,37 @@
+SB = {};
+
+function configProxy() {
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+        for (key in changes) {
+            localconfig[key] = changes[key].newValue;
+        }
+    });
+    var handler = {
+        set: function(obj, prop, value) {
+            chrome.storage.sync.set({
+                [prop]: value
+            })
+        },
+        get: function(obj, prop) {
+            return localconfig[prop]
+        }
+    };
+    return new Proxy({}, handler);
+}
+
+fetchConfig = _ => new Promise(function(resolve, reject) {
+    chrome.storage.sync.get(null, function(items) {
+        localconfig = items;  // Data is ready
+        resolve();
+    });
+});
+
+async function config() {
+    localconfig = {};
+    await fetchConfig();
+    SB.config = configProxy();
+}
+
 // Function that can be used to wait for a condition before returning
 async function wait(condition, timeout = 5000, check = 100) { 
     return await new Promise((resolve, reject) => {
