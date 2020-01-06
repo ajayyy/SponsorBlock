c99f7925eb34058a9c75abbbd35167952e6bf150
Ajay Ramachandran <dev@ajay.app>
2020-01-06 16:13:50 -0500

    Fixed migrate

--- SB.js
+++ SB.js
@@ -28,7 +28,7 @@ fetchConfig = () => new Promise((resolve, reject) => {
 
 function migrate() { // Convert sponsorTimes format
     for (key in SB.localconfig) {
-        if (key.startsWith("sponsortime")) {
+        if (key.startsWith("sponsorTimes")) {
             SB.config.sponsorTimes.set(key.substr(12), SB.config[key]);
             delete SB.config[key];
         }
