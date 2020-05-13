dd5ed6ce421fd98ba994814ceb786790c2dea706
Rafael <15365765+rafern@users.noreply.github.com>
2020-05-13 17:45:08 +0100

    Remove duplicate code

--- src/content.ts
+++ src/content.ts
@@ -469,7 +469,7 @@ function startSponsorSchedule(includeIntersectingSegments: boolean = false, curr
     cancelSponsorSchedule();
 
     // Don't skip if advert playing and reset last checked time
-    if (document.getElementsByClassName('ad-showing').length > 0) {
+    if (isAdPlaying) {
         // Reset lastCheckVideoTime
         lastCheckVideoTime = -1;
         lastCheckTime = 0;
