48861439b7ed4b27e77a684c98f456489de6fc58
Ajay Ramachandran <dev@ajay.app>
2020-07-05 00:21:26 -0400

    Don't run time without skips on mobile or invidious

--- src/content.ts
+++ src/content.ts
@@ -1574,6 +1574,8 @@ function updateAdFlag() {
 }
 
 function showTimeWithoutSkips(allSponsorTimes): void {
+    if (onMobileYouTube || onInvidious) return;
+
 	let skipDuration = 0;
 	
 	// Calculate skipDuration based from the segments in the preview bar
@@ -1587,7 +1589,7 @@ function showTimeWithoutSkips(allSponsorTimes): void {
 	
 	// YouTube player time display
 	let display = document.getElementsByClassName("ytp-time-display notranslate")[0];
-	if (display === undefined) return
+	if (!display) return;
 	
     let formatedTime = utils.getFormattedTime(video.duration - skipDuration);
 	
