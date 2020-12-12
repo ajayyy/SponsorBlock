105c148ccc2ee589dc47716a963835687013d683
Max Baumann <info@fosefx.com>
2020-12-12 22:56:31 +0100

    feat(lint): add default .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {},
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};
