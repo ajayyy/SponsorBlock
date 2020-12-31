ece66522e58000d40f69a98d1592343c83028c5c
Ajay Ramachandran <dev@ajay.app>
2020-12-30 21:46:05 -0500

    Shrink youcap text on Firefox

--- src/components/SubmissionNoticeComponent.tsx
+++ src/components/SubmissionNoticeComponent.tsx
@@ -131,7 +131,7 @@ class SubmissionNoticeComponent extends React.Component<SubmissionNoticeProps, S
 
         return (
             <tr style={{textAlign: "center"}}>
-                <p style={{width: "300px", textAlign: "center", display: "inline-block"}}>
+                <p style={{width: "300px", textAlign: "center", display: "inline-block", fontSize: "11px"}}>
                     Like contributing to crowdsourced projects? 
                     Consider checking out <a href="https://youcap.video/" style={{textDecoration: "underline"}}>YouCap</a>,
                     a new open-source replacement for YouTube{"'"}s now defunct community captions.
