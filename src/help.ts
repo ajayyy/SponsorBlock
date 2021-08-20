b3336a218e5b5fd7bb828ad6c2b022670d45638a
Ajay Ramachandran <dev@ajay.app>
2021-08-20 02:00:15 -0400

    Make help page localisable and make up to date
    Closes https://github.com/ajayyy/SponsorBlock/issues/509

--- src/help.ts
+++ src/help.ts
@@ -1,8 +1,16 @@
+import Config from "./config";
 import { showDonationLink } from "./utils/configUtils";
+
+import Utils from "./utils";
+const utils = new Utils();
 
 window.addEventListener('DOMContentLoaded', init);
 
 async function init() {
+    utils.localizeHtmlPage();
+
+    await utils.wait(() => Config.config !== null);
+
     if (!showDonationLink()) {
         document.getElementById("sbDonate").style.display = "none";
     }
