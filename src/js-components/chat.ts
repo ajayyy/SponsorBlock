1a717542c77438292104c8e5d7d1e70ff041af4f
Ajay Ramachandran <dev@ajay.app>
2021-09-28 20:05:48 -0400

    Improve warning message

--- src/js-components/chat.ts
+++ src/js-components/chat.ts
@@ -31,14 +31,16 @@ export function openChat(config: ChatConfig): void {
 }
 
 export async function openWarningChat(warningMessage: string): Promise<void> {
+    const warningReasonMatch = warningMessage.match(/Warning reason: '(.+)'/);
+    alert(chrome.i18n.getMessage("warningChatInfo") + `\n\n${warningReasonMatch ? ` Warning reason: ${warningReasonMatch[1]}` : ``}`);
+
     const userNameData = await utils.asyncRequestToServer("GET", "/api/getUsername?userID=" + Config.config.userID);
     const userName = userNameData.ok ? JSON.parse(userNameData.responseText).userName : "";
     const publicUserID = await utils.getHash(Config.config.userID);
-    const warningReasonMatch = warningMessage.match(/Warning reason: '(.+)'/);
 
     openChat({
         displayName: `${userName ? userName : ``}${userName !== publicUserID ? ` | ${publicUserID}` : ``}`,
-        composerInitialValue: `I got a warning and want to know what I need to do to improve.` +
+        composerInitialValue: `I got a warning and confirm I [REMOVE THIS CAPITAL TEXT TO CONFIRM] reread the guidelines.` +
                                 warningReasonMatch ? ` Warning reason: ${warningReasonMatch[1]}` : ``,
         customDescription: chrome.i18n.getMessage("warningChatInfo")
     });
