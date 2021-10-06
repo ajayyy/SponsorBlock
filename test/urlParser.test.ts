4045978b5492b9cdbb87f688e234430fd03131c1
Ajay Ramachandran <dev@ajay.app>
2021-10-05 20:51:20 -0400

    Support older browsers

--- test/urlParser.test.ts
+++ test/urlParser.test.ts
@@ -5,15 +5,15 @@ describe("getStartTimeFromUrl", () => {
         expect(getStartTimeFromUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=123")).toBe(123);
     });
 
-    it("parses with a seconds", () => {
+    it("parses with seconds", () => {
         expect(getStartTimeFromUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=123s")).toBe(123);
     });
 
-    it("parses with a minutes", () => {
+    it("parses with minutes", () => {
         expect(getStartTimeFromUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=23m3s")).toBe(23 * 60 + 3);
     });
 
-    it("parses with a hours", () => {
+    it("parses with hours", () => {
         expect(getStartTimeFromUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1h2m3s")).toBe(1 * 60 * 60 + 2 * 60 + 3);
     });
 
