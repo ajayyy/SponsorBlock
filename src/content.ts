3da6a57e42211c41cd28ffcdd8194eceeb3ab74a
Michael C <michael@mchang.name>
2022-05-10 21:56:01 -0400

    shorcut creating preview bar on invidious

--- src/content.ts
+++ src/content.ts
@@ -391,6 +391,16 @@ function handleMobileControlsMutations(): void {
 function createPreviewBar(): void {
     if (previewBar !== null) return;
 
+    if (onInvidious) { // shortut invidious
+        // skip isVisible check for invidious
+        const el = document.querySelector<HTMLElement>(".vjs-progress-holder");
+        if (el) {
+            previewBar = new PreviewBar(el, onMobileYouTube, onInvidious);
+            updatePreviewBar();
+            return;
+        }
+    }
+
     const progressElementSelectors = [
         // For mobile YouTube
         ".progress-bar-background",
