c7f254db7037d906a7e2ffcbfe5af7a34c472cf7
Ajay Ramachandran <dev@ajay.app>
2022-07-03 00:24:28 -0400

    Use templates

--- src/options.ts
+++ src/options.ts
@@ -599,8 +599,8 @@ function downloadConfig() {
     const file = document.createElement("a");
     const jsonData = JSON.parse(JSON.stringify(Config.cachedSyncConfig));
     const dateTimeString = new Date().toJSON().replace("T", "_").replace(/:/g, ".").replace(/.\d+Z/g, "")
-    file.setAttribute("href", "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(jsonData)));
-    file.setAttribute("download", "SponsorBlockConfig_"+dateTimeString+".json");
+    file.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(jsonData))}`);
+    file.setAttribute("download", `SponsorBlockConfig_${dateTimeString}.json`);
     document.body.append(file);
     file.click();
     file.remove();
