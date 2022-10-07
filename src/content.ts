bb7f069254a4b8d342ee1425d81cf0a2fe8750c3
Ajay <dev@ajay.app>
2022-10-07 12:22:39 -0400

    Only check import duplicates against unsubmitted segments

--- src/content.ts
+++ src/content.ts
@@ -276,7 +276,7 @@ function messageListener(request: Message, sender: unknown, sendResponse: (respo
             const importedSegments = importTimes(request.data, video.duration);
             let addedSegments = false;
             for (const segment of importedSegments) {
-                if (!sponsorTimesSubmitting.concat(sponsorTimes ?? []).some(
+                if (!sponsorTimesSubmitting.some(
                         (s) => Math.abs(s.segment[0] - segment.segment[0]) < 1
                             && Math.abs(s.segment[1] - segment.segment[1]) < 1)
                         && (segment.category !== "chapter" || utils.getCategorySelection("chapter"))) {
