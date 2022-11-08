ce3f77ed20a3fd12ca5fa2ff0d26e0a75b30928f
Ajay <dev@ajay.app>
2022-11-07 21:15:04 -0500

    Better key moments check by not importing when no 0 second chapter

--- src/utils/pageUtils.ts
+++ src/utils/pageUtils.ts
@@ -71,8 +71,7 @@ export function getExistingChapters(currentVideoID: VideoID, duration: number): 
 
     const chapters: SponsorTime[] = [];
     // .ytp-timed-markers-container indicates that key-moments are present, which should not be divided
-    if (chaptersBox && !(getControls()?.parentElement?.parentElement
-            ?.querySelector(".ytp-timed-markers-container")?.childElementCount > 0)) {
+    if (chaptersBox) {
         let lastSegment: SponsorTime = null;
         const links = chaptersBox.querySelectorAll("ytd-macro-markers-list-item-renderer > a");
         for (const link of links) {
@@ -102,6 +101,11 @@ export function getExistingChapters(currentVideoID: VideoID, duration: number): 
             lastSegment.segment[1] = duration;
             chapters.push(lastSegment);
         }
+    }
+
+    if (chapters[0] && chapters[0].segment[0] !== 0) {
+        // This is key moments instead of chapters, don't import as they are not full sections
+        return [];
     }
 
     return chapters;
