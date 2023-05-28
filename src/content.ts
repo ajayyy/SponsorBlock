1b2be35bd60b5a328090563257c1eb788b364a5d
Ajay <dev@ajay.app>
2023-05-28 16:11:49 -0400

    Check multiple times if preview bar failed to be added

--- src/content.ts
+++ src/content.ts
@@ -1323,6 +1323,8 @@ function videoElementChange(newVideo: boolean): void {
     checkPreviewbarState();
 
     // Incase the page is still transitioning, check again in a few seconds
+    setTimeout(checkPreviewbarState, 100);
+    setTimeout(checkPreviewbarState, 1000);
     setTimeout(checkPreviewbarState, 5000);
 }
 
