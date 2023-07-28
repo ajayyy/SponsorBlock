fc3710b37b14052ada393882cc863e05c6943c4a
Ajay <dev@ajay.app>
2023-07-28 18:39:51 -0400

    Clear preview bar on update

--- src/utils/pageCleaner.ts
+++ src/utils/pageCleaner.ts
@@ -1,6 +1,6 @@
 export function cleanPage() {
     // For live-updates
-    for (const element of document.querySelectorAll("#categoryPillParent, .playerButton, .sponsorThumbnailLabel, #submissionNoticeContainer, .sponsorSkipNoticeContainer, #sponsorBlockPopupContainer, .skipButtonControlBarContainer")) {
+    for (const element of document.querySelectorAll("#categoryPillParent, .playerButton, .sponsorThumbnailLabel, #submissionNoticeContainer, .sponsorSkipNoticeContainer, #sponsorBlockPopupContainer, .skipButtonControlBarContainer, #previewbar")) {
         element.remove();
     }
 }
