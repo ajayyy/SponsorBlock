90bb9a4d02caae05248d66e5db6fabc0941b6b91
Ajay <dev@ajay.app>
2023-07-28 20:44:19 -0400

    Remove log

--- src/content.ts
+++ src/content.ts
@@ -1594,7 +1594,6 @@ function skipToTime({v, skipTime, skippingSegments, openNotice, forceAutoSkip, u
                     v.currentTime = 0;
                 } else if (v.duration > 1 && skipTime[1] >= v.duration
                         && (navigator.vendor === "Apple Computer, Inc." || isPlayingPlaylist())) {
-                    console.log("doing workaround")
                     // MacOS will loop otherwise #1027
                     // Sometimes playlists loop too #1804
                     v.currentTime = v.duration - 0.001;
