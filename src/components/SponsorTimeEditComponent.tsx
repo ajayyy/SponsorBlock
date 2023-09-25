109b7ed5bc8254983d157c62594a3b2f65466941
Ajay <dev@ajay.app>
2023-09-24 21:13:43 -0400

    Don't show start button for highlight

--- src/components/SponsorTimeEditComponent.tsx
+++ src/components/SponsorTimeEditComponent.tsx
@@ -128,11 +128,13 @@ class SponsorTimeEditComponent extends React.Component<SponsorTimeEditProps, Spo
                     style={timeDisplayStyle}
                     className="sponsorTimeDisplay">
 
-                        <span id={"startButton" + this.idSuffix}
-                            className="sponsorNowButton"
-                            onClick={() => this.setTimeTo(0, 0)}>
-                                {chrome.i18n.getMessage("bracketStart")}
-                        </span>
+                        {sponsorTime.actionType !== ActionType.Poi ? (
+                            <span id={"startButton" + this.idSuffix}
+                                className="sponsorNowButton"
+                                onClick={() => this.setTimeTo(0, 0)}>
+                                    {chrome.i18n.getMessage("bracketStart")}
+                            </span>
+                        ): ""}
 
                         <span id={"nowButton0" + this.idSuffix}
                             className="sponsorNowButton"
