ae685baeef026658078fc010d8d014d3755a1b92
Ajay <dev@ajay.app>
2023-10-01 13:56:34 -0400

    Fix tip ui broken for submissions

--- src/content.ts
+++ src/content.ts
@@ -2317,7 +2317,7 @@ async function sendSubmitMessage() {
         playerButtons.submit.button.style.animation = "unset";
         playerButtons.submit.image.src = chrome.extension.getURL("icons/PlayerUploadFailedIconSponsorBlocker.svg");
 
-        if (response.status === 403 && response.responseText.startsWith("Submission rejected due to a warning from a moderator.")) {
+        if (response.status === 403 && response.responseText.startsWith("Submission rejected due to a tip from a moderator.")) {
             openWarningDialog(skipNoticeContentContainer);
         } else {
             alert(getErrorMessage(response.status, response.responseText));
